c62129bae30473fd8514c4528824d297e5ff002e
Baniukiewicz <baniuk@zut.edu.pl>
2013-09-26 12:31:33 +0200

    testy pod Aspenem - ok

--- MSVC/A2F/VersionNo.h
+++ MSVC/A2F/VersionNo.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           0.5.267.322
+#define VERSION_FULL           0.5.268.323
 
 #define VERSION_BASEYEAR       2013
-#define VERSION_DATE           "2013-09-25"
-#define VERSION_TIME           "16:08:32"
+#define VERSION_DATE           "2013-09-26"
+#define VERSION_TIME           "11:11:39"
 
 #define VERSION_MAJOR          0
 #define VERSION_MINOR          5
-#define VERSION_BUILDNO        267
-#define VERSION_EXTEND         322
+#define VERSION_BUILDNO        268
+#define VERSION_EXTEND         323
 
-#define VERSION_FILE           0,5,267,322
-#define VERSION_PRODUCT        0,5,267,322
-#define VERSION_FILESTR        "0,5,267,322"
-#define VERSION_PRODUCTSTR     "0,5,267,322"
+#define VERSION_FILE           0,5,268,323
+#define VERSION_PRODUCT        0,5,268,323
+#define VERSION_FILESTR        "0,5,268,323"
+#define VERSION_PRODUCTSTR     "0,5,268,323"
 
 #endif
