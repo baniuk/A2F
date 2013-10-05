62bcf92d86d59851e0efb592a2e2b40699df4117
Baniukiewicz <baniuk@zut.edu.pl>
2013-10-05 15:11:57 +0200

    Added empty projest under ASPEN for test

---
+++ MSVC/A2F/InfoDialog.h
@@ -0,0 +1,59 @@
+// InfoDialog.h : Declaration of the CInfoDialog
+
+#pragma once
+
+#include "resource.h"       // main symbols
+
+#include <atlhost.h>
+
+using namespace ATL;
+
+// CInfoDialog
+
+class CInfoDialog : 
+	public CAxDialogImpl<CInfoDialog>
+{
+public:
+	CInfoDialog()
+	{
+	}
+
+	~CInfoDialog()
+	{
+	}
+
+	enum { IDD = IDD_INFODIALOG };
+
+BEGIN_MSG_MAP(CInfoDialog)
+	MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+	COMMAND_HANDLER(IDOK, BN_CLICKED, OnClickedOK)
+	COMMAND_HANDLER(IDCANCEL, BN_CLICKED, OnClickedCancel)
+	CHAIN_MSG_MAP(CAxDialogImpl<CInfoDialog>)
+END_MSG_MAP()
+
+// Handler prototypes:
+//  LRESULT MessageHandler(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+//  LRESULT CommandHandler(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled);
+//  LRESULT NotifyHandler(int idCtrl, LPNMHDR pnmh, BOOL& bHandled);
+
+	LRESULT OnInitDialog(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		CAxDialogImpl<CInfoDialog>::OnInitDialog(uMsg, wParam, lParam, bHandled);
+		bHandled = TRUE;
+		return 1;  // Let the system set the focus
+	}
+
+	LRESULT OnClickedOK(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled)
+	{
+		EndDialog(wID);
+		return 0;
+	}
+
+	LRESULT OnClickedCancel(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled)
+	{
+		EndDialog(wID);
+		return 0;
+	}
+};
+
+
