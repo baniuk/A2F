f794e8b8d96edc67000e0ec3f7f705f68bdfc1ba
Baniukiewicz <baniuk@zut.edu.pl>
2013-12-03 12:39:16 +0100

    Fixed property sheets. Now every functionality must have its own property.

--- MSVC/A2F/VersionNo.h
+++ MSVC/A2F/VersionNo.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           0.9.277.340
+#define VERSION_FULL           0.9.336.346
 
 #define VERSION_BASEYEAR       2013
-#define VERSION_DATE           "2013-10-05"
-#define VERSION_TIME           "15:59:55"
+#define VERSION_DATE           "2013-12-03"
+#define VERSION_TIME           "12:34:20"
 
 #define VERSION_MAJOR          0
 #define VERSION_MINOR          9
-#define VERSION_BUILDNO        277
-#define VERSION_EXTEND         340
+#define VERSION_BUILDNO        336
+#define VERSION_EXTEND         346
 
-#define VERSION_FILE           0,9,277,340
-#define VERSION_PRODUCT        0,9,277,340
-#define VERSION_FILESTR        "0,9,277,340"
-#define VERSION_PRODUCTSTR     "0,9,277,340"
+#define VERSION_FILE           0,9,336,346
+#define VERSION_PRODUCT        0,9,336,346
+#define VERSION_FILESTR        "0,9,336,346"
+#define VERSION_PRODUCTSTR     "0,9,336,346"
 
 #endif
--- MSVC/A2F/stdafx.h
+++ MSVC/A2F/stdafx.h
@@ -32,8 +32,8 @@
 // adds decoding HRESULT errors (winstl::error_desc_a(err_code))
 #include <winstl/error/error_desc.hpp>
 // add config4cpp headers
-#include <config4cpp/Configuration.h>
-#include <config4cpp/SchemaValidator.h>
+//#include <config4cpp/Configuration.h>
+//#include <config4cpp/SchemaValidator.h>
 #include <iostream>
 #include <vector>
 #include <windows.h>
