813364df96432cef7cfbc95e4b62553abbc40d41
Baniukiewicz <baniuk@zut.edu.pl>
2014-02-07 21:25:26 +0100

    #14 FluentStart moved to separate project, FluentStarter finished

--- MSVC/FluentStart/C_Properties.cpp
+++ MSVC/FluentStart/C_Properties.cpp
@@ -4,11 +4,11 @@
  * \details This properties will be later moved to external file
  * \author PB
  * \date 2014/02/06
- * \warning
  */
 #include "stdafx.h"
 #include "C_Properties.h"
 
+/// path to main starter exe. This starter spawns next processes
 const ATL::CString C_Properties::PAR_PROCNAME = _T("ff");
 /// run 3dpp version of fluent
 ATL::CString C_Properties::PAR_PARNAME = _T(" 3ddp");
