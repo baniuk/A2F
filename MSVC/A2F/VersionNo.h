30f1bc20ae9e8ef64225cd8d63c270dded7c28c4
Baniukiewicz <baniuk@zut.edu.pl>
2014-02-11 22:54:30 +0100

    #9 Revorked Calculate method, added error handling, not tested

--- MSVC/A2F/VersionNo.h
+++ MSVC/A2F/VersionNo.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           1.0.1038.434
+#define VERSION_FULL           1.0.1041.438
 
 #define VERSION_BASEYEAR       2013
-#define VERSION_DATE           "2014-02-08"
-#define VERSION_TIME           "15:06:33"
+#define VERSION_DATE           "2014-02-11"
+#define VERSION_TIME           "22:52:24"
 
 #define VERSION_MAJOR          1
 #define VERSION_MINOR          0
-#define VERSION_BUILDNO        1038
-#define VERSION_EXTEND         434
+#define VERSION_BUILDNO        1041
+#define VERSION_EXTEND         438
 
-#define VERSION_FILE           1,0,1038,434
-#define VERSION_PRODUCT        1,0,1038,434
-#define VERSION_FILESTR        "1,0,1038,434"
-#define VERSION_PRODUCTSTR     "1,0,1038,434"
+#define VERSION_FILE           1,0,1041,438
+#define VERSION_PRODUCT        1,0,1041,438
+#define VERSION_FILESTR        "1,0,1041,438"
+#define VERSION_PRODUCTSTR     "1,0,1041,438"
 
 #endif
--- MSVC/FluentStart/C_Properties.cpp
+++ MSVC/FluentStart/C_Properties.cpp
@@ -9,7 +9,7 @@
 #include "C_Properties.h"
 
 /// path to main starter exe. This starter spawns next processes
-const ATL::CString C_Properties::PAR_PROCNAME = _T("D:\\Programy\\ANSYS Inc\\v145\\fluent\\ntbin\\win64\\fluent.exe");
+const ATL::CString C_Properties::PAR_PROCNAME = _T("f:\\Program Files\\ANSYS Inc\\v145\\fluent\\ntbin\\ntx86\\fluent.exe");
 /// run 3dpp version of fluent
 ATL::CString C_Properties::PAR_PARNAME = _T(" 3ddp"); // 3ddp -g -i ";
 /// name of the subprocess: fl12016s.exe
