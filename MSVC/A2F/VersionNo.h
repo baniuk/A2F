342a31e9a0def6b1afe5cab015b1e97d16149d2e
Baniukiewicz <baniuk@zut.edu.pl>
2014-04-07 20:55:15 +0200

    #20 Zmiany w strukturze pre i pos build

--- MSVC/A2F/VersionNo.h
+++ MSVC/A2F/VersionNo.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           1.0.1092.448
+#define VERSION_FULL           1.0.1096.449
 
 #define VERSION_BASEYEAR       2013
-#define VERSION_DATE           "2014-04-03"
-#define VERSION_TIME           "20:26:42"
+#define VERSION_DATE           "2014-04-07"
+#define VERSION_TIME           "20:27:21"
 
 #define VERSION_MAJOR          1
 #define VERSION_MINOR          0
-#define VERSION_BUILDNO        1092
-#define VERSION_EXTEND         448
+#define VERSION_BUILDNO        1096
+#define VERSION_EXTEND         449
 
-#define VERSION_FILE           1,0,1092,448
-#define VERSION_PRODUCT        1,0,1092,448
-#define VERSION_FILESTR        "1,0,1092,448"
-#define VERSION_PRODUCTSTR     "1,0,1092,448"
+#define VERSION_FILE           1,0,1096,449
+#define VERSION_PRODUCT        1,0,1096,449
+#define VERSION_FILESTR        "1,0,1096,449"
+#define VERSION_PRODUCTSTR     "1,0,1096,449"
 
 #endif
